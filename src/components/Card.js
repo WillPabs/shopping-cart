f22a32794f22c426f7089e45f13d9965011e3f6e
Will <pabiterowilliam@yahoo.com>
2022-12-30 01:30:25 -0500

    add api

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,13 +7,14 @@ const Card = ({ image, name, description, onClick, addToCart }) => {
                 <figure onClick={onClick}>
                     <img src={image} alt="productImage" />
                     <figcaption>{name}</figcaption>
+                    <input
+                        type="number"
+                        id="quantity"
+                        name="quantity"
+                        value="1"
+                    />
+                    <button type="submit">Add To Cart</button>
                 </figure>
-                <input
-                    type="number"
-                    id="quantity"
-                    name="quantity"
-                />
-                <button type="submit">Add To Cart</button>
             </div>
         </form>
     )
