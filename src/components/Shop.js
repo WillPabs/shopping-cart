70d8367fb3b1ecde5c4b07877dfd656e44e3bab4
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-04 14:17:51 -0500

    import useParams

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,26 +1,26 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 import Card from "./Card";
 
-const Shop = ({ productList }) => {
-    const [products, setProducts] = useState(productList);
-    console.log("prods", productList)
-    const productCards = products.length > 0 ? products.map(product => {
-        return (
-            <li key={product.id}>
-                <Card name={product.name} image={product.image}/>
-            </li>
-        )
-    }) : <div>No Products</div>;
+const Shop = () => {
+    const { productType } = useParams();
 
+    // const productCards = products.length > 0 ? products.map(product => {
+    //     return (
+    //         <li key={product.id}>
+    //             <Card name={product.name} image={product.image}/>
+    //         </li>
+    //     )
+    // }) : <div>No Products</div>;
 
     return (
         // if productlist is empty then we display the list of categories to choose from
         // otherwise we display the productslist
         <div>
-            <h2>Browse {products[0].category} Products</h2>
-            
+            {/* <h2>Browse {products[0].category} Products</h2> */}
+            <h2>{productType}</h2>
             <ul id="product-list">
-                {productCards}
+                {/* {productCards} */}
                 {/* {productsList.map(product => {
                     return (
                         <div style={{width:80}}>
