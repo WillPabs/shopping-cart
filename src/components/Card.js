bf47108eea82fc2d044784265c3f50dc8dc4032d
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-04 16:58:05 -0500

    add price and style img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
-const Card = ({ image, name, description, onClick, addToCart }) => {
-
+const Card = ({ image, name, price, description, onClick, addToCart }) => {
     
     return (
         <form>
             <div>
                 <figure onClick={onClick}>
-                    <img src={image} alt="productImage" />
-                    <figcaption>{name}</figcaption>
+                    <img style={{width:120}} src={image} alt="productImage" />
+                    <figcaption>
+                        <h4>{name}</h4>
+                        <h4>${price}</h4>
+                    </figcaption>
                     <input
                         type="number"
                         id="quantity"
