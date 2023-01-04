3ff0487174eaa8f58d0467cefb7d37b35922c4e8
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-04 16:51:35 -0500

    fetch product type and display as Cards

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,26 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "./Card";
 
 const Shop = () => {
     const { productType } = useParams();
+    const [products, setProducts] = useState([]);
 
-    // const productCards = products.length > 0 ? products.map(product => {
-    //     return (
-    //         <li key={product.id}>
-    //             <Card name={product.name} image={product.image}/>
-    //         </li>
-    //     )
-    // }) : <div>No Products</div>;
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/category/${productType}`)
+            .then(data => {
+                data.json().then(products => {
+                    setProducts(products);
+                })
+            })
+        
+    }, []);
+
+    const productCards = products.length > 0 ? products.map(product => {
+        return (
+            <li key={product.id}>
+                <Card name={product.title} price={product.price} image={product.image}/>
+            </li>
+        )
+    }) : <div>No Products</div>;
 
     return (
         // if productlist is empty then we display the list of categories to choose from
         // otherwise we display the productslist
         <div>
-            {/* <h2>Browse {products[0].category} Products</h2> */}
-            <h2>{productType}</h2>
+            <h2>Browse {productType} Products</h2>
             <ul id="product-list">
-                {/* {productCards} */}
+                {console.log(products)}
+                {productCards}
                 {/* {productsList.map(product => {
                     return (
                         <div style={{width:80}}>
