f0d6415a940e39608a6a4f54bc8463c9076cbe3d
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-05 14:51:45 -0500

    remove product state and added productCards state

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,34 +2,32 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "./Card";
 import Categories from "./Categories";
+import Header from "./Header";
 
 const Shop = () => {
     const { productType } = useParams();
-    const [products, setProducts] = useState([]);
+    const [productCards, setProductCards] = useState();
 
     useEffect(() => {
         fetch(`https://fakestoreapi.com/products/category/${productType}`)
             .then(data => {
                 data.json().then(products => {
-                    setProducts(products);
+                    setProductCards(products.length > 0 ? products.map(product => {
+                        return (
+                            <li key={product.id}>
+                                <Card name={product.title} price={product.price} image={product.image}/>
+                            </li>
+                        )
+                    }) : <Categories/>);
                 })
             })
-        
-    }, []);
-
-    const productCards = products.length > 0 ? products.map(product => {
-        return (
-            <li key={product.id}>
-                <Card name={product.title} price={product.price} image={product.image}/>
-            </li>
-        )
-    }) : <Categories/>;
+    }, [productType]);
 
     return (
         <div>
-            <h2>Browse {productType} Products</h2>
+            <Header/>
+            <h2>Browse {productType ? productType : 'All'} Products</h2>
             <ul id="product-list">
-                {console.log(products)}
                 {productCards}
             </ul>
         </div>
