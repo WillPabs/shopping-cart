40d254df97c170512fb418cbf3e2e42c0f144964
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-06 17:07:38 -0500

    add dependency and mongodb connection

--- src/services/cart.api.js
+++ src/services/cart.api.js
@@ -1,6 +1,13 @@
+import express from "express";
+
+const cartRoutes = express.Router();
+
 const cartApi = () => {
     
     // create cart
+    cartRoutes.route('/cart').get((req, res) => {
+        // const dbConnect = dbo.getDb();
+    })
 
     // add to cart
 
