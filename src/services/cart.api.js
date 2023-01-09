f1fcec9ca937ccca8c8edbf1c619372cc0a05355
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-09 15:35:29 -0500

    add get request

--- src/services/cart.api.js
+++ src/services/cart.api.js
@@ -1,17 +1,26 @@
 import express from "express";
+import db from "../db/conn";
 
 const cartRoutes = express.Router();
 
-const cartApi = () => {
+// const cartApi = () => {
     
     // create cart
-    cartRoutes.route('/cart').get((req, res) => {
-        // const dbConnect = dbo.getDb();
-    })
+    cartRoutes.get('/cart', (req, res) => {
+        const dbConnect = db.getDb();
+        dbConnect.collection('sales').find({}).limit(20)
+        .toArray((err, result) => {
+            if (err) {
+                res.status(400).send('Error fetching sales!');
+            } else {
+                res.json(result);
+            }
+        });
+    });
 
     // add to cart
 
     // delete from cart
 
     // checkout
-}
+// }
