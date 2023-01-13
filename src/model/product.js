523bfddf01a6303eb697a41f45301874b47b0418
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-13 13:47:53 -0500

    add category property

--- src/model/product.js
+++ src/model/product.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
+    category: {
+        required: true,
+        type: String,
+    },
     name : {
         required: true,
         type: String
