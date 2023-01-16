c1b623ade017d6ec34a3822862796d7109eac445
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-16 15:01:33 -0500

    use CartItem component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-
+import CartItem from "./CartItem";
 
 const Cart = () => {
     const [products, setProducts] = useState([]);
@@ -9,13 +9,10 @@ const Cart = () => {
     const getCart = async () => {
         const data = await fetch('http://localhost:3001/cart');
         const cart = await data.json();
-        console.log(cart);
         let price = 0;
         await cart.forEach(p => {
-            console.log(p)
             price += Number(p.price);
         })
-        console.log(price);
         setTotalPrice(price);
         setProducts(cart);
     }
@@ -30,9 +27,7 @@ const Cart = () => {
             <ul id='cart-list'>
                 {products.map((p) => {
                     return (
-                        <li key={p._id}>
-                            <p>{p.title} x {p.requestedQuantity}</p>
-                        </li>
+                        <CartItem key={p._id} product={p}/>
                     )
                 })}
                 {'Total: $'+totalPrice}
