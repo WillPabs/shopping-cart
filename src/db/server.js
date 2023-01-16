551cccb3887e3cf6414c969ceec6c1d60c1d6540
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-16 15:56:23 -0500

    fix CORS blocked

--- src/db/server.js
+++ src/db/server.js
@@ -26,7 +26,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', cartRouter);
