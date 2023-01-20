ec75eb3532c28c735716563c40490eef86388c90
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-20 15:18:40 -0500

    add prettier dependency

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,79 +1,72 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import {
   Navbar,
   MobileNav,
   Typography,
   Button,
   IconButton,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react'
 
 const Nav = () => {
-    const [openNav, setOpenNav] = useState(false);
- 
-    useEffect(() => {
-        window.addEventListener(
-        "resize",
-        () => window.innerWidth >= 960 && setOpenNav(false)
-        );
-    }, []);
+  const [openNav, setOpenNav] = useState(false)
 
-    const logo = '#';
-    const companyName = 'ShoppingCart';
-    // button will toggle showing the cart component
-    const cartButton = <button type="button">
-        <img src="#" alt="Cart" />
-    </button>
+  useEffect(() => {
+    window.addEventListener(
+      'resize',
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    )
+  }, [])
 
-    const loginButton = <button className="" type="button">Login</button>
+  const logo = '#'
 
-    const navList = (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-          <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-          >
-            <Link to="/" className="flex items-center">
-              Home
-            </Link>
-          </Typography>
-          <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-          >
-            <Link to="/about" className="flex items-center">
-              About
-            </Link>
-          </Typography>
-          <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-          >
-            <Link to="/shop" className="flex items-center">
-              Shop
-            </Link>
-          </Typography>
-          <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-          >
-            <Link to="/contact" className="flex items-center">
-              Contact
-            </Link>
-          </Typography>
-        </ul>
-      );
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="/" className="flex items-center">
+          Home
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="/about" className="flex items-center">
+          About
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="/shop" className="flex items-center">
+          Shop
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="/contact" className="flex items-center">
+          Contact
+        </Link>
+      </Typography>
+    </ul>
+  )
 
-    return (   
-        <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+  return (
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
@@ -133,46 +126,44 @@ const Nav = () => {
       </MobileNav>
     </Navbar>
 
+    // <nav className="relative
+    // w-full
+    // flex flex-wrap
+    // items-center
+    // justify-between
+    // py-4
+    // bg-gray-100
+    // text-gray-500
+    // hover:text-gray-700
+    // focus:text-gray-700
+    // shadow-lg
+    // navbar navbar-expand-lg navbar-light">
+    //     <div id="logo-container" className="flex flex-row space-x-4 justify-center">
+    //         <img src={logo} alt="logoImage" />
+    //         <h3>{companyName}</h3>
+    //     </div>
+    //     <ul id="links-container" className="flex flex-row space-x-4 justify-center">
+    //         <li className="nav-link">
+    //             <Link to='/'>Home</Link>
+    //         </li>
+    //         <li className="nav-link">
+    //             <Link to='/shop'>Shop</Link>
+    //         </li>
+    //         <li className="nav-link">
+    //             <Link to='/about'>About</Link>
+    //         </li>
+    //         <li className="nav-link">
+    //             <Link to='/contact'>Contact</Link>
+    //         </li>
+    //         <li className="nav-link">
+    //             {cartButton}
+    //         </li>
+    //         <li className="nav-link">
+    //             {loginButton}
+    //         </li>
+    //     </ul>
+    // </nav>
+  )
+}
 
-        // <nav className="relative
-        // w-full
-        // flex flex-wrap
-        // items-center
-        // justify-between
-        // py-4
-        // bg-gray-100
-        // text-gray-500
-        // hover:text-gray-700
-        // focus:text-gray-700
-        // shadow-lg
-        // navbar navbar-expand-lg navbar-light">
-        //     <div id="logo-container" className="flex flex-row space-x-4 justify-center">
-        //         <img src={logo} alt="logoImage" />
-        //         <h3>{companyName}</h3>
-        //     </div>
-        //     <ul id="links-container" className="flex flex-row space-x-4 justify-center">              
-        //         <li className="nav-link">
-        //             <Link to='/'>Home</Link>
-        //         </li>
-        //         <li className="nav-link">
-        //             <Link to='/shop'>Shop</Link>
-        //         </li>
-        //         <li className="nav-link">
-        //             <Link to='/about'>About</Link>
-        //         </li>
-        //         <li className="nav-link">
-        //             <Link to='/contact'>Contact</Link>
-        //         </li>
-        //         <li className="nav-link">
-        //             {cartButton}
-        //         </li>
-        //         <li className="nav-link">
-        //             {loginButton}
-        //         </li>
-        //     </ul>
-        // </nav>
-    )
-};
-
-export default Nav;
-
+export default Nav
