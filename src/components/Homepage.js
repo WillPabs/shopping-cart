8c76d77f41be34c6e12b54cbadf65e034e962162
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-24 13:56:59 -0500

    add banner

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,24 @@
-import Nav from './Nav';
-import Categories from './Categories';
+import Nav from "./Nav";
+import Categories from "./Categories";
+import { Typography } from "@material-tailwind/react";
 
 const Homepage = () => {
-    return (
-        <div id="homepage">
-            <Nav/>
-            <Categories/>
-        </div>
-    )
-}
+	const banner = (
+		<Typography
+			variant="h1"
+			className="justify-center text-center mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4"
+		>
+			Welcome to Copped! Browse Your Desired Item and Cop It!
+		</Typography>
+	);
+
+	return (
+		<div id="homepage">
+			<Nav />
+			{banner}
+			<Categories />
+		</div>
+	);
+};
 
 export default Homepage;
