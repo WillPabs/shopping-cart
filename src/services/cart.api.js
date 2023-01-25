97a4f39fc7010ab6851294977fe1279b60e96358
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-25 15:30:43 -0500

    update /cart post request to modify quantity when item is already in cart

--- src/services/cart.api.js
+++ src/services/cart.api.js
@@ -3,78 +3,92 @@ const db = require("../db/conn");
 
 const cartRoutes = express.Router();
 
-// const cartApi = () => {
-    
-    // create cart
-    cartRoutes.get('/cart', async (req, res, next) => {
-        try {
-            await db.connectToServer();
-            const dbConnect = await db.getDb();
-    
-            const cart = await dbConnect.collection('cart').find({}).toArray();
-            res.json(cart);
-        } catch (error) {
-            console.log(error);
-            res.json(error);
-        }
-    });
+// get cart
+cartRoutes.get("/cart", async (req, res, next) => {
+	try {
+		await db.connectToServer();
+		const dbConnect = await db.getDb();
 
-    // add to cart
-    cartRoutes.post('/cart', async (req, res) => {
-        try {
-            await db.connectToServer();
-            const dbConnect = await db.getDb();
-    
-            const item = req.body;
-    
-            const insertRes = await dbConnect.collection('cart').insertOne(item);
-            console.log(item);
-            res.json(insertRes);
-        } catch (error) {
-            console.log(error);
-            res.json(error);
-        }
-    })
+		const cart = await dbConnect.collection("cart").find({}).toArray();
+		res.json(cart);
+	} catch (error) {
+		console.log(error);
+		res.json(error);
+	}
+});
 
-    // delete from cart
-    cartRoutes.patch('/cart', async (req, res) => {
-        try {
-            await db.connectToServer();
-            const dbConnect = await db.getDb();
+// add to cart
+cartRoutes.post("/cart", async (req, res) => {
+	try {
+		await db.connectToServer();
+		const dbConnect = await db.getDb();
 
-            const item = req.body;
+		const item = req.body;
 
-            const cart = await dbConnect.collection('cart');
+		const cartItem = await dbConnect
+			.collection("cart")
+			.find({ productId: item.productId })
+			.toArray();
 
-            const deleteRes = await cart.deleteOne(item);
-            
-            console.log(deleteRes);
-            res.json(deleteRes);
-        } catch (error) {
-            console.log(error);
-            res.json(error);
-        }
-    });
+		if (Object.keys(await cartItem).length > 0) {
+			console.log("### ITEM IN CART");
+			const query = { productId: item.productId };
+			const update = {
+				$inc: {
+					requestedQuantity: item.requestedQuantity,
+				},
+			};
+			await dbConnect.collection("cart").updateOne(query, update);
+		} else {
+			const insertRes = await dbConnect
+				.collection("cart")
+				.insertOne(item);
+			res.json(insertRes);
+		}
+	} catch (error) {
+		console.log(error);
+		res.json(error);
+	}
+});
 
-    // clear cart
-    cartRoutes.delete('/cart', async (req, res) => {
-        try {
-            await db.connectToServer();
-            const dbConnect = await db.getDb();
+// delete from cart
+cartRoutes.patch("/cart", async (req, res) => {
+	try {
+		await db.connectToServer();
+		const dbConnect = await db.getDb();
 
-            const cart = await dbConnect.collection('cart');
+		const item = req.body;
 
-            const deletedRes = await cart.drop();
-            console.log(deletedRes);
-            res.json(deletedRes);
+		const cart = await dbConnect.collection("cart");
 
-        } catch (error) {
-            console.log(error);
-            res.json(error);
-        }
-    })
+		const deleteRes = await cart.deleteOne(item);
 
-    // checkout
+		console.log(deleteRes);
+		res.json(deleteRes);
+	} catch (error) {
+		console.log(error);
+		res.json(error);
+	}
+});
+
+// clear cart
+cartRoutes.delete("/cart", async (req, res) => {
+	try {
+		await db.connectToServer();
+		const dbConnect = await db.getDb();
+
+		const cart = await dbConnect.collection("cart");
+
+		const deletedRes = await cart.drop();
+		console.log(deletedRes);
+		res.json(deletedRes);
+	} catch (error) {
+		console.log(error);
+		res.json(error);
+	}
+});
+
+// checkout
 // }
 
 module.exports = cartRoutes;
