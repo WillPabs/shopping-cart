0564379981687b09a23d6c311db4d9f1a0aa03aa
Will Pabs <pabiterowilliam@yahoo.com>
2023-01-27 14:52:45 -0500

    change banner

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,7 +8,7 @@ const Homepage = () => {
 			variant="h1"
 			className="justify-center text-center mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4"
 		>
-			Welcome to Copped! Browse Your Desired Item and Cop It!
+			Welcome to Copped! Like It, Click It, Cop It!
 		</Typography>
 	);
 
