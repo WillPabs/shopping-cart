d8cc4a81489134cd86b7542e9357edba46395c2c
Will Pabs <pabiterowilliam@yahoo.com>
2023-03-20 15:28:12 -0400

    add footer template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Homepage from "./components/Homepage";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Homepage/>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<Homepage />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import { Typography } from "@material-tailwind/react";
+
+const Footer = () => {
+	return (
+		<div id="footer">
+			<div>Home</div>
+			<div>Shop</div>
+			<div>About</div>
+			<div>Contact</div>
+			<div>Account</div>
+		</div>
+	);
+};
+
+export default Footer;
