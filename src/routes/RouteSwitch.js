4383755a63cee7bf59ac571728107a886b644d80
Will Pabs <pabiterowilliam@yahoo.com>
2023-03-27 15:13:27 -0400

    add 404 route

--- src/routes/RouteSwitch.js
+++ src/routes/RouteSwitch.js
@@ -2,24 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "../App";
 import Nav from "../components/Nav";
+import NotFound from "../components/NotFound";
 import Shop from "../components/Shop";
 
 const RouteSwitch = () => {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App/>}></Route>
-                <Route 
-                    path='/shop/:productType' 
-                    element={<Shop/>}
-                >
-                </Route>
-                <Route path='/shop' element={<Shop/>}></Route>
-                <Route path='/Nav' element={<Nav/>}></Route>
-            </Routes>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}></Route>
+				<Route path="/shop/:productType" element={<Shop />}></Route>
+				<Route path="/shop" element={<Shop />}></Route>
+				<Route path="/Nav" element={<Nav />}></Route>
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default RouteSwitch;
