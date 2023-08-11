2ff15e88733d5fa2d780daa773977dc54c5cef6f
Will Pabs <pabiterowilliam@yahoo.com>
2023-08-11 12:46:17 -0400

    removed publicPath from webpack config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RouteSwitch from "./routes/RouteSwitch";
-import Homepage from "./components/Homepage";
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Homepage />
 		<RouteSwitch />
 	</React.StrictMode>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,46 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    devServer: {
-        historyApiFallback: true,
-    },    
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: 'public/index.html',
-        }),
-        new webpack.ProvidePlugin({
-            'React':     'react'
-          })
-      ],
-    module:{
-        rules:[
-            {
-                test: /\.(avif|png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                loader: 'babel-loader',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                options: { 
-                    presets: [
-                        '@babel/env',
-                        ['@babel/preset-react', {'runtime': 'automatic'}]
-                    ] 
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                  'style-loader',
-                  'css-loader',
-                  'postcss-loader'
-                ]
-            }
-        ]
-    },
-}
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: "/",
+	},
+	devServer: {
+		historyApiFallback: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "public/index.html",
+		}),
+		new webpack.ProvidePlugin({
+			React: "react",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(avif|png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				loader: "babel-loader",
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				options: {
+					presets: [
+						"@babel/env",
+						["@babel/preset-react", { runtime: "automatic" }],
+					],
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+		],
+	},
+};
