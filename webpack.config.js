3d1b304d1659c0206f1a0b487e59d445d1440303
Will <pabiterowilliam@yahoo.com>
2023-08-15 14:18:46 -0400

    update webpack config mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-	mode: "development",
+	mode: "production",
 	entry: "./src/index.js",
 	output: {
 		filename: "main.js",
