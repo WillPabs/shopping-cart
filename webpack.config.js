e8cf856cdc11c2424b16b99b562ca54dbfd36ebf
Will Pabs <pabiterowilliam@yahoo.com>
2023-08-16 12:07:23 -0400

    updated config

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-	mode: "production",
+	mode: "development",
 	entry: "./src/index.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
-		publicPath: "/",
+		clean: true,
 	},
 	devServer: {
 		historyApiFallback: true,
