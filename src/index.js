3bbfa087cb7ea4c8d8412197640c5b35cbef6d27
Will Pabs <pabiterowilliam@yahoo.com>
2023-08-16 12:53:22 -0400

    added scripts for gh-pages deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import "./styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Homepage />
 		<RouteSwitch />
 	</React.StrictMode>
 );
